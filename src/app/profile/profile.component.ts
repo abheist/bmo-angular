cec72da6c71922447e2c362f5bb40417eb7517bd
Abhishek <abhishek291093@gmail.com>
2020-01-06 20:12:32 +0530

    AKS | integrated user with backend api

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,29 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerService } from '../server.service';
 import { AuthService } from '../auth.service';
+import { MatTableDataSource } from '@angular/material/table';
 
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface TableDataStructure {
+  id: number;
+  username: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  is_active: boolean;
+  is_staff: boolean;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
 
 @Component({
   selector: 'app-profile',
@@ -36,14 +24,20 @@ export class ProfileComponent implements OnInit {
 
   constructor(private server: ServerService, private authService: AuthService) { }
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['id', 'username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff'];
+  dataSource: MatTableDataSource<TableDataStructure>;
 
   ngOnInit() {
     this.server.request('GET', '/api/v1/rest-auth/user/').subscribe((user: any) => {
       if (user) {
         this.personalName = user.first_name + user.last_name;
         this.personalEmail = user.email;
+      }
+    });
+
+    this.server.request('GET', '/api/v1/users/').subscribe((data: any) => {
+      if (data) {
+        this.dataSource = data.results;
       }
     });
   }
