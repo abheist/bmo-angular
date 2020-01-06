2d20108969001ab8102ca7308ce973c735d55353
Abhishek <abhishek291093@gmail.com>
2020-01-06 18:29:44 +0530

    AKS | comfirm email | request | done |

--- src/app/confirm-email/confirm-email.component.ts
+++ src/app/confirm-email/confirm-email.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerService } from '../server.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-confirm-email',
@@ -7,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmEmailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private server: ServerService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.server.request('GET', '/api/v1/verify-email/' + this.route.snapshot.params.id + '/')
+      .subscribe((response: any) => { });
   }
 }
