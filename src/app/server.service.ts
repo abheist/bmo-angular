9f654f8e262ca015cb45af358c4520aafec3ad8c
Abhishek <abhishek291093@gmail.com>
2020-01-14 20:06:37 +0530

    AKS | added production env

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
-const baseUrl = 'http://localhost:8000';
+const baseUrl = environment.baseUrl;
 
 @Injectable({
   providedIn: 'root'
